3e18414eb06b210d74efee80e2d5dfb5f1c8b885
giuliaputin <gputinfrances@tudelft.nl>
2025-03-13 09:47:40 +0100

    hi

--- Plotting_Model/Annual_Model_Measurements/Given_Plotting.py
+++ Plotting_Model/Annual_Model_Measurements/Given_Plotting.py
@@ -12,30 +12,31 @@
 # — Flávio Quadros, 05/02/2020
 #    - updated 07/03/2024
 
-# Import modules
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import xarray as xr
 
 # Open DataSet and print an overview of it
-ds = xr.open_dataset('U:/AviationAQ/BSc/2024/data/model/Aerosol.JAN.OFF.nc4')
+ds = xr.open_dataset('PM25.annual.ON.nc4')
 print(ds)
 
 # Select a DataArray
 var = 'PM25'
 da = ds[var]
 
-# Select data from ground level at a specific time
-daSurf = da.isel(lev=0).sel(time='2019-01-15')
+# Since there's no "lev" or "time" dimension, use the dataset directly
+daSurf = da
 
 # Print the mean value of this selection
-print(f'\nMean of {var} is = {float(daSurf.mean().values):.1f} {da.attrs["units"]}')
+print(f'\nMean of {var} is = {float(daSurf.mean().values):.1f}')
 
-# Plot figure using matplotlib and cartopy. The use of some cosmetic options is illustrated here
+# Plot figure using matplotlib and cartopy
 fig = plt.figure(figsize=[12, 6])
 ax = plt.axes(projection=ccrs.EqualEarth(central_longitude=10))
 ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5, edgecolor='darkgrey')
 ax.coastlines(resolution='50m', linewidth=0.5, color='white')
+
+# Plot the data
 daSurf.plot(transform=ccrs.PlateCarree(), vmin=0, vmax=50)
 plt.show()
