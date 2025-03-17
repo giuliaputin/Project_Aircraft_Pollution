b4eaf861fb5bb531561a5be902eac9b9bcaa792f
akudohtudelft <akudohliang@tudelft.nl>
2025-03-17 09:37:49 +0100

    made subtraction

--- model_animations/aerosol_animation.py
+++ model_animations/aerosol_animation.py
@@ -6,9 +6,10 @@ import xarray as xr
 import os
 import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
+from difference_machine import differencer
 
 # Select either July (JUL) or January (JAN)
-month = 'JUL'
+month = 'JAN'
 
 # select aviation ON or OFF
 aviation = 'ON'
@@ -17,11 +18,11 @@ aviation = 'ON'
 # Choose between: ['PM25', 'AerMassNIT', 'AerMassNH4', 'AerMassPOA', 'AerMassBC']
 var = 'PM25'
 
-level = 71
+level = 1
 # --------------------------------------------------------------------------------------------------------------------
 # Starting of the preprocessing, no need to modify anything after this
 # Open DataSet and print an overview of it
-ds = xr.open_dataset(os.path.join('raw_data', 'model', f'Aerosol.{month}.{aviation}.nc4'))
+ds = xr.open_dataset(os.path.join(os.path.dirname(__file__),"..",'raw_data', 'model', f'Aerosol.{month}.{aviation}.nc4'))
 
 da = ds[var]
 
@@ -31,12 +32,13 @@ ax = plt.axes(projection=ccrs.EqualEarth(central_longitude=10))
 ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5, edgecolor='darkgrey')
 ax.coastlines(resolution='50m', linewidth=0.5, color='white')
 
-
+average = False
 # Function to update the plot for each time step
 def update(frame):
 
     # Select the data for the current time step
-    daSurf = da.isel(lev=level).isel(time=frame)
+    
+    daSurf = differencer("Aerosol",month,var,frame,level,average)
     
     # Clear the previous plot
     ax.clear()
@@ -46,10 +48,10 @@ def update(frame):
     ax.coastlines(resolution='50m', linewidth=0.5, color='white')
     
     # Plot the data for the current time step
-    im = daSurf.plot(ax=ax, transform=ccrs.PlateCarree(), vmin=0, vmax=50, add_colorbar=False)
-    
+    im = daSurf.plot(ax=ax, transform=ccrs.PlateCarree(), add_colorbar=False)
+    #vmin=0, vmax=50
     # Add a title with the current time
-    ax.set_title(f"{var} at time {da.time[frame].values}, lev = {daSurf.lev.values}", fontsize=14)
+    ax.set_title(f"{var} at time {da.time[frame].values}", fontsize=14)
     
     return [im]
 
--- model_animations/difference_machine.py
+++ model_animations/difference_machine.py
@@ -7,39 +7,55 @@ import cartopy
 import cartopy.crs as ccrs #projections list
 import cartopy.feature as cfeature
 
-#for venv use
+# for venv use
 print(sys.executable)
+
+def differencer(type, month, var, frame, level, average=False):
+    dsoff = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,"raw_data", "model", f"{type}.{month}.OFF.nc4") )
+    dson = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,"raw_data", "model", f"{type}.{month}.ON.nc4") )
+    daoff = dsoff[var]
+    daon = dson[var]
+    if average:
+        daSurfoff = daoff.isel(time=frame).mean(dim = "lev")
+        daSurfon = daon.isel(time=frame).mean(dim = "lev")
+    else:
+        daSurfoff = daoff.isel(time=frame).isel(lev=level)
+        daSurfon = daon.isel(time=frame).isel(lev=level)
+    daDiff = daSurfon - daSurfoff
+
+    return daDiff
 
 
 # Some defaults:
 
 plt.rcParams['figure.figsize'] = (12, 5)  # Default plot size
 
-dsoff = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,"raw_data", "model", "Aerosol.Jan.OFF.nc4") )
-dson = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,"raw_data", "model", "Aerosol.Jan.ON.nc4") )
+# dsoff = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,"raw_data", "model", "Aerosol.JAN.OFF.nc4") )
+# dson = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,"raw_data", "model", "Aerosol.JAN.ON.nc4") )
+    
 
-offPM = dsoff.PM25
-onPM = dson.PM25
-
-
-#print(offPM.isel(time=0))
-altsumoff = offPM.mean(dim="time").isel(lev=71)
-altsumon = onPM.mean(dim="time").isel(lev=71)
-
-#print(altsumoff)
-
-print("Bananas")
-
-diff = altsumon - altsumoff
+# offPM = dsoff["PM25"]
+# onPM = dson["PM25"]
 
 
-# Define the map projection (how does the map look like)
-ax = plt.axes(projection=ccrs.EqualEarth())
-# ax is an empty plot. We now plot the variable sw_avg onto ax
-diff.plot(ax=ax, transform=ccrs.PlateCarree()) 
-# the keyword "transform" tells the function in which projection the data is stored 
-ax.coastlines(); ax.gridlines(); ax.add_feature(cfeature.BORDERS.with_scale('50m')); # Add gridlines and coastlines to the plot
-plt.show()
+# #print(offPM.isel(time=0))
+# altsumoff = offPM.isel(time=0).isel(lev=71)
+# altsumon = onPM.isel(time=0).isel(lev=71)
+
+# print("Bananas")
+
+# diff = altsumon - altsumoff
+
+# diff = differencer("Aerosol", "JAN", "PM25", 0, 71)
+
+
+# # Define the map projection (how does the map look like)
+# ax = plt.axes(projection=ccrs.EqualEarth())
+# # ax is an empty plot. We now plot the variable sw_avg onto ax
+# diff.plot(ax=ax, transform=ccrs.PlateCarree()) 
+# # the keyword "transform" tells the function in which projection the data is stored 
+# ax.coastlines(); ax.gridlines(); ax.add_feature(cfeature.BORDERS.with_scale('50m')); # Add gridlines and coastlines to the plot
+# plt.show()
 
 # # Define the map projection (how does the map look like)
 # ax = plt.axes(projection=ccrs.EqualEarth())
