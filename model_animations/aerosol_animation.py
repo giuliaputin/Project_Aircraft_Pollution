023f08be1b6a547cd632a8907981451e0ac8f939
Federico Angioni <f.angioni@student.tudelft.nl>
2025-03-17 09:27:08 +0100

    animation aerosol & NO2

--- model_animations/aerosol_animation.py
+++ model_animations/aerosol_animation.py
@@ -6,7 +6,9 @@ import xarray as xr
 import os
 import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
+import numpy as np
 
+# From here you can set up the animation:
 # Select either July (JUL) or January (JAN)
 month = 'JUL'
 
@@ -17,7 +19,14 @@ aviation = 'ON'
 # Choose between: ['PM25', 'AerMassNIT', 'AerMassNH4', 'AerMassPOA', 'AerMassBC']
 var = 'PM25'
 
-level = 71
+# Select the level you want to animate [0, 72]
+level = 0
+
+# If average is set to True then the level variable above is overridden
+# Average with respect to height is taken
+average = False
+
+
 # --------------------------------------------------------------------------------------------------------------------
 # Starting of the preprocessing, no need to modify anything after this
 # Open DataSet and print an overview of it
@@ -34,29 +43,55 @@ ax.coastlines(resolution='50m', linewidth=0.5, color='white')
 
 # Function to update the plot for each time step
 def update(frame):
+    if average:
+        # Select the data for the current time step
+        daSurf = da.mean(dim= "lev").isel(time=frame)
+        
+        # Clear the previous plot
+        ax.clear()
+        
+        # Redraw the coastlines and borders for every frame
+        ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5, edgecolor='darkgrey')
+        ax.coastlines(resolution='50m', linewidth=0.5, color='white')
+        
+        # Plot the data for the current time step
+        im = daSurf.plot(ax=ax, transform=ccrs.PlateCarree(), vmin=0, vmax=50, add_colorbar=False)
 
-    # Select the data for the current time step
-    daSurf = da.isel(lev=level).isel(time=frame)
-    
-    # Clear the previous plot
-    ax.clear()
-    
-    # Redraw the coastlines and borders for every frame
-    ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5, edgecolor='darkgrey')
-    ax.coastlines(resolution='50m', linewidth=0.5, color='white')
-    
-    # Plot the data for the current time step
-    im = daSurf.plot(ax=ax, transform=ccrs.PlateCarree(), vmin=0, vmax=50, add_colorbar=False)
-    
-    # Add a title with the current time
-    ax.set_title(f"{var} at time {da.time[frame].values}, lev = {daSurf.lev.values}", fontsize=14)
-    
+        date_str = np.datetime_as_string(da.time[frame].values, unit='D') 
+        # Add a title with the current time
+        ax.set_title(f"{var} at time {date_str}, averaged over height", fontsize=14)
+        
+    else:
+        # Select the data for the current time step
+        daSurf = da.isel(lev = level).isel(time=frame)
+        
+        # Clear the previous plot
+        ax.clear()
+        
+        # Redraw the coastlines and borders for every frame
+        ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5, edgecolor='darkgrey')
+        ax.coastlines(resolution='50m', linewidth=0.5, color='white')
+        
+        # Plot the data for the current time step
+        im = daSurf.plot(ax=ax, transform=ccrs.PlateCarree(), vmin=0, vmax=50, add_colorbar=False)
+        
+        
+        date_str = np.datetime_as_string(da.time[frame].values, unit='D') 
+        # Add a title with the current time
+        ax.set_title(f"{var} at time {date_str}, level = {np.round(daSurf.lev.values, 3)}", fontsize=14)
+        
     return [im]
 
 # Create the animation
 ani = FuncAnimation(fig, update, frames=len(da.time), interval=500, blit=False)
 
 # Display the animation
+
 plt.show()
 
-# ani.save(os.path.join('model_animations', 'animations', f'{month}_{aviation}_{var}.mp4'), writer='ffmpeg', fps=4)
+# Saving of the files
+if average:
+    ani.save(os.path.join('model_animations', 'animations', f'{month}_{aviation}_{var}_avg.mp4'), writer='ffmpeg', fps=4)
+else:
+    ani.save(os.path.join('model_animations', 'animations', f'{month}_{aviation}_{var}_lev{level}.mp4'), writer='ffmpeg', fps=4)
+
