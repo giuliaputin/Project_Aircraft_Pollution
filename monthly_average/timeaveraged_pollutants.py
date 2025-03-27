cd59b9958fb5aa67a1a696919965a4bc9b3de49a
akudohtudelft <akudohliang@tudelft.nl>
2025-03-27 10:39:54 +0100

    unified the axes within comparisons

--- monthly_average/timeaveraged_pollutants.py
+++ monthly_average/timeaveraged_pollutants.py
@@ -21,18 +21,29 @@ for type in types:
     for var in types[type]:
         fig, ax = plt.subplots(1, 2, figsize= [12, 4], subplot_kw={"projection": ccrs.EqualEarth(central_longitude=10)})
         plt.tight_layout()
-    
+
+        daSurf = [[],[]]
         for i, month in enumerate(months):
 
-            daSurf = differencer(type, month, var)
+            daSurf[i] = differencer(type, month, var)
+
+            if i==0:
+                vmin = float(daSurf[i].values.min())
+                vmax = float(daSurf[i].values.max())
+            else:
+                vmin = min(vmin, float(daSurf[i].values.min()))
+                vmax = max(vmax, float(daSurf[i].values.max()))
+
+        for i, month in enumerate(months):
+
             ax[i].add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5, edgecolor='darkgrey')
             ax[i].coastlines(resolution='50m', linewidth=0.5, color='black')
 
             # Plot the data for the current time step
-            daSurf.plot(ax=ax[i], transform=ccrs.PlateCarree(), add_colorbar=False)
+            daSurf[i].plot(ax=ax[i], transform=ccrs.PlateCarree(), vmin=vmin, vmax=vmax)
 
             # Add a title with the current time
-            ax[i].set_title(f"{var}, monthly average {month}, level = {np.round(daSurf.lev.values, 3)}", fontsize=14)
+            ax[i].set_title(f"{var}, monthly average {month}, level = {np.round(daSurf[i].lev.values, 3)}", fontsize=14)
         
        
         plt.savefig(os.path.join(os.path.dirname(__file__), '..', 'monthly_average', 'pollutants_figures',f'{type}_{var}_timeaveraged.png'))
