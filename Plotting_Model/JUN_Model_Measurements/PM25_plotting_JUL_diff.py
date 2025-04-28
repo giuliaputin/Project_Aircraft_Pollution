ef9c196401e21d889773c0fd812d27b85e75de6e
VulomKiir <ruighlev@gmail.com>
2025-04-28 11:40:21 +0200

    finish fixing file referencing

--- Plotting_Model/JUN_Model_Measurements/PM25_plotting_JUL_diff.py
+++ Plotting_Model/JUN_Model_Measurements/PM25_plotting_JUL_diff.py
@@ -7,7 +7,7 @@ import os
 
 # Open the datasets
 ds_on = xr.open_dataset(os.path.join(os.path.dirname(__file__), ".", 'PM25.annual.ON.nc4'))   # Aviation ON
-ds_off = xr.open_dataset(os.path.join(os.path.dirname(__file__), ".", 'PM25.annual.ON.nc4'))  # Aviation OFF
+ds_off = xr.open_dataset(os.path.join(os.path.dirname(__file__), ".", 'PM25.annual.OFF.nc4'))  # Aviation OFF
 
 print(ds_on)
 
