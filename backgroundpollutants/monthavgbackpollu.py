8f6df442b22a6d6b21dbaf7fae49bddb29afe8a2
akudohtudelft <akudohliang@tudelft.nl>
2025-05-08 10:14:14 +0200

    background pollution plots for thomas and joao

---
+++ backgroundpollutants/monthavgbackpollu.py
@@ -0,0 +1,55 @@
+# Import modules
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import xarray as xr
+import os
+import numpy as np
+
+# Types and variables that will be plotted
+types = {'Aerosol': ['PM25', 'AerMassNIT', 'AerMassNH4', 'AerMassPOA', 'AerMassBC', 'AerMassSO4'],
+                  'O3' : ['SpeciesConc_O3'],
+                  'NO2' : ['SpeciesConc_NO2']}
+               
+# --------------------------------------------------------------------------------------------------------------------
+# Starting of the preprocessing, no need to modify anything after this
+
+months = ['JAN', 'JUL']
+
+# For loop to iterate through each type and through each variable withing each type
+for type in types:
+    for var in types[type]:
+        fig, ax = plt.subplots(1, 2, figsize= [12, 4], subplot_kw={"projection": ccrs.EqualEarth(central_longitude=10)})
+        plt.tight_layout()
+
+        daSurf = [[],[]]
+        for i, month in enumerate(months):
+
+            dsoff = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,  "raw_data", "model", f"{type}.{month}.OFF.nc4") )
+            daoff = dsoff[var]
+            daSurfoff = daoff.isel(lev=0).mean(dim = "time")
+            daSurf[i] = daSurfoff
+
+            if i==0:
+                vmin = float(daSurf[i].values.min())
+                vmax = float(daSurf[i].values.max())
+            else:
+                vmin = min(vmin, float(daSurf[i].values.min()))
+                vmax = max(vmax, float(daSurf[i].values.max()))
+
+        for i, month in enumerate(months):
+
+            ax[i].add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5, edgecolor='darkgrey')
+            ax[i].coastlines(resolution='50m', linewidth=0.5, color='black')
+
+            # Plot the data for the current time step
+            daSurf[i].plot(ax=ax[i], transform=ccrs.PlateCarree(), vmin=vmin, vmax=vmax)
+
+            # Add a title with the current time
+            ax[i].set_title(f"{var}, monthly average {month}, Surface, Bckg", fontsize=14)
+        
+       
+        plt.savefig(os.path.join(os.path.dirname(__file__), '..', 'backgroundpollutants', 'pollutants_figures',f'{type}_{var}_timeaveragedbackground.png'))
+        
+# Uncomment to display the plots on screen
+# plt.show()
