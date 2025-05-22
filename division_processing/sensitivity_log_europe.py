13948a99374e039bafd777c59909ba14c693a638
akudohtudelft <akudohliang@tudelft.nl>
2025-05-22 10:16:51 +0200

    made single plots work (kinda, negative values don't work yet) and added percentile scale.

--- division_processing/sensitivity_log_europe.py
+++ division_processing/sensitivity_log_europe.py
@@ -19,7 +19,6 @@ shpfilename = shpreader.natural_earth(
 reader = shpreader.Reader(shpfilename)
 records = reader.records()
 
-IQR = False
 europe_countries = [
     "Austria",
     "Belgium",
@@ -110,6 +109,8 @@ types = {
 }
 
 
+percentile = 0
+
 units = [1e-3, 44.6 * 48 * 1e-3, 44.6 * 46.01 * 1e-3]
 
 emissions = xr.open_dataset(
@@ -127,137 +128,132 @@ for i, values in enumerate(types.items()):
     type_, vars = values
 
     for var, emittants in vars.items():
-        measures = []
 
         for month in months:
             pollutant = differencer(type_, month, var) * units[i] * area["AREA"] * 128
             sum_emittants = adder(emissions, emittants)
-            pollutant_top20 = np.nanpercentile(pollutant.values, 0)
-            select_pollutant = pollutant.where(
-                pollutant.values >= pollutant_top20, np.nan
+
+            measure = pollutant / sum_emittants
+
+            fig, ax = plt.subplots(
+                1,1,
+                figsize=[12, 8],
+                subplot_kw={"projection": ccrs.PlateCarree()},
+                constrained_layout=True,
             )
 
-            measure = select_pollutant / sum_emittants
+            # for i, month in enumerate(months):
+            ax.add_feature(
+                cfeature.BORDERS.with_scale("10m"), linewidth=0.5, edgecolor="darkgrey"
+            )
+            ax.coastlines(resolution="10m", linewidth=0.5, color="black")
 
-            measures.append(measure)
-
-        fig, ax = plt.figure(
-            figsize=[12, 8],
-            subplot_kw={"projection": ccrs.PlateCarree()},
-            constrained_layout=True,
-        )
-
-        # for i, month in enumerate(months):
-        ax.add_feature(
-            cfeature.BORDERS.with_scale("10m"), linewidth=0.5, edgecolor="darkgrey"
-        )
-        ax.coastlines(resolution="10m", linewidth=0.5, color="black")
-
-        # Set extent to Europe
-        ax.set_extent(
-            [
-                EUROPE_BOUNDS["lon_min"],
-                EUROPE_BOUNDS["lon_max"],
-                EUROPE_BOUNDS["lat_min"],
-                EUROPE_BOUNDS["lat_max"],
-            ],
-            crs=ccrs.PlateCarree(),
-        )
-
-        # Add bounding box on map
-        europe_rect = Rectangle(
-            (EUROPE_BOUNDS["lon_min"], EUROPE_BOUNDS["lat_min"]),
-            EUROPE_BOUNDS["lon_max"] - EUROPE_BOUNDS["lon_min"],
-            EUROPE_BOUNDS["lat_max"] - EUROPE_BOUNDS["lat_min"],
-            linewidth=1.5,
-            edgecolor="blue",
-            facecolor="none",
-            linestyle="--",
-            transform=ccrs.PlateCarree(),
-        )
-        # ax[i].add_patch(europe_rect)
-
-        # Optional: Add gridlines with labels
-        gl = ax.gridlines(
-            draw_labels=True, linewidth=0.3, color="gray", alpha=0.5, linestyle="--"
-        )
-        gl.top_labels = False
-        gl.right_labels = False
-        gl.xlabel_style = {"size": 9}
-        gl.ylabel_style = {"size": 9}
-
-        masked_measure = mask_ocean(measures[i])
-        negative_measures = abs(masked_measure.where(masked_measure.values < 0))
-        masked_measure_europe = masked_measure.where(masked_measure.values > 0)
-
-        # print(min(negative_measures.lat))
-        # print(min(masked_measure_europe.lat))
-
-        if IQR:
-            median = np.nanmedian(masked_measure_europe.values)
-            q1 = np.nanpercentile(masked_measure_europe.values, 25)
-            q3 = np.nanpercentile(masked_measure_europe.values, 75)
-            iqr = q3 - q1
-            lower_bound = q1 - 1.5 * iqr
-            upper_bound = q3 + 1.5 * iqr
-            masked_measure_europe = masked_measure_europe.where(
-                (masked_measure_europe >= lower_bound)
-                & (masked_measure_europe <= upper_bound),
-                np.nan,
+            # Set extent to Europe
+            ax.set_extent(
+                [
+                    EUROPE_BOUNDS["lon_min"],
+                    EUROPE_BOUNDS["lon_max"],
+                    EUROPE_BOUNDS["lat_min"],
+                    EUROPE_BOUNDS["lat_max"],
+                ],
+                crs=ccrs.PlateCarree(),
             )
 
-        vmin = np.nanmin(masked_measure_europe.values)
-        vmax = np.nanmax(masked_measure_europe.values)
-        norm1 = matplotlib.colors.LogNorm(vmin=vmin, vmax=vmax)
-
-        # fig.set_size_inches(12, 6)
-        if np.any(~np.isnan(negative_measures.values)):
-            norm2 = matplotlib.colors.LogNorm(
-                vmin=np.nanmin(negative_measures.values),
-                vmax=np.nanmax(negative_measures.values),
-            )
-            negative_measures.plot(
-                ax=ax[i],
-                cmap="BuGn",
+            # Add bounding box on map
+            europe_rect = Rectangle(
+                (EUROPE_BOUNDS["lon_min"], EUROPE_BOUNDS["lat_min"]),
+                EUROPE_BOUNDS["lon_max"] - EUROPE_BOUNDS["lon_min"],
+                EUROPE_BOUNDS["lat_max"] - EUROPE_BOUNDS["lat_min"],
+                linewidth=1.5,
+                edgecolor="blue",
+                facecolor="none",
+                linestyle="--",
                 transform=ccrs.PlateCarree(),
-                norm=norm2,
+            )
+            # ax[i].add_patch(europe_rect)
+
+            # Optional: Add gridlines with labels
+            gl = ax.gridlines(
+                draw_labels=True, linewidth=0.3, color="gray", alpha=0.5, linestyle="--"
+            )
+            gl.top_labels = False
+            gl.right_labels = False
+            gl.xlabel_style = {"size": 9}
+            gl.ylabel_style = {"size": 9}
+
+            masked_measure = mask_ocean(measure)
+            negative_measures = abs(masked_measure.where(masked_measure.values < 0))
+            positive_measures = masked_measure.where(masked_measure.values > 0)
+
+            positive_measures_cutoff = np.nanpercentile(positive_measures.values, percentile)
+            positive_measures_topx = measure.where(
+                positive_measures.values >= positive_measures_cutoff, np.nan
+            )
+
+            negative_measures_cutoff = np.nanpercentile(negative_measures.values, percentile)
+            negative_measures_topx = measure.where(
+                negative_measures.values >= negative_measures_cutoff, np.nan
+            )
+
+            # print(min(negative_measures.lat))
+            # print(min(masked_measure_europe.lat))
+
+            vmin = np.nanmin(positive_measures_topx.values)
+            vmax = np.nanmax(positive_measures_topx.values)
+            norm1 = matplotlib.colors.LogNorm(vmin=vmin, vmax=vmax)
+
+            # fig.set_size_inches(12, 6)
+            if np.any(~np.isnan(negative_measures_topx.values)):
+                norm2 = matplotlib.colors.LogNorm(
+                    vmin=np.nanmin(negative_measures_topx.values),
+                    vmax=np.nanmax(negative_measures_topx.values),
+                )
+                negative_measures_topx.plot(
+                    ax=ax,
+                    cmap="BuGn",
+                    transform=ccrs.PlateCarree(),
+                    norm=norm2,
+                    cbar_kwargs={
+                        "label": "Negative values (log scale)",
+                        # "aspect": 40,
+                        "location": "bottom",
+                        "pad": 0.01,
+                        # "shrink": 0.5,
+                    },
+                )
+
+            positive_measures_topx.plot(
+                ax=ax,
+                transform=ccrs.PlateCarree(),
+                norm=norm1,
+                cmap="YlOrRd",
                 cbar_kwargs={
-                    "label": "Negative values (log scale)",
+                    "label": "Positive value (log scale)",
                     # "aspect": 40,
                     "location": "bottom",
-                    # "pad": 0.05,
+                    "pad": 0.01,
                     # "shrink": 0.5,
                 },
             )
 
-        masked_measure_europe.plot(
-            ax=ax[i],
-            transform=ccrs.PlateCarree(),
-            norm=norm1,
-            cmap="YlOrRd",
-            cbar_kwargs={
-                "label": "Absolute value (log scale)",
-                # "aspect": 40,
-                "location": "bottom",
-                # "pad": 0.05,
-                # "shrink": 0.5,
-            },
-        )
-
-        # ax[i].set_title(f"{var}, monthly average {month}", fontsize=14)
-        # plt.tight_layout()
-        ax[i].set_title("")
-
-        plt.savefig(
-            os.path.join(
-                os.path.dirname(__file__),
-                "..",
-                "division_processing",
-                "division_figures",
-                "analysis",
-                f"{type_}_{var}_{month}_timeaveraged_logscale.png",
+            # ax[i].set_title(f"{var}, monthly average {month}", fontsize=14)
+            # plt.tight_layout()
+            ax.set_title("")
+            if percentile > 0:
+                ifpercen = "_top" + str(100 - percentile)
+            else:
+                ifpercen = ""
+            
+            plt.savefig(
+                os.path.join(
+                    os.path.dirname(__file__),
+                    "..",
+                    "division_processing",
+                    "division_figures",
+                    "analysis",
+                    f"{type_}_{var}_{month}_timeaveraged_logscale{ifpercen}.png",
+                )
             )
-        )
 
 end = time.time()
 plt.show()
