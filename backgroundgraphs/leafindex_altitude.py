e636e53017c4995f4fae48c31b69e4971976462f
akudohtudelft <akudohliang@tudelft.nl>
2025-06-02 11:24:35 +0200

    made font size bigger, higher resolution png

--- backgroundgraphs/leafindex_altitude.py
+++ backgroundgraphs/leafindex_altitude.py
@@ -1,4 +1,5 @@
 # Import modules
+import matplotlib
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
@@ -7,14 +8,19 @@ import os
 import numpy as np
 
 miscer = ["Met_LAI", "Met_PHIS"]
-plotargs = [["Leaf Area Index", "BuGn", "leafareaindex"], ["Surface Geopotential Height", "terrain", "altitude"]]
+plotargs = [["Leaf Area Index", "BuGn", "leafareaindex"], ["Surface Geopotential Height [m]", "terrain", "altitude"]]
 
 # Which one would you like to plot: 0 Leaf Index, 1 Altitude
-ploter = 1
+ploter = 0
 
 # For loop to iterate through each type and through each variable withing each type
 fig, ax = plt.subplots(1, 1, figsize= [9, 7], subplot_kw={"projection": ccrs.EqualEarth(central_longitude=10)})
 plt.tight_layout()
+
+font = {'family' : 'DejaVu Sans',
+        'size'   : 20}
+
+matplotlib.rc('font', **font)
 
 ds = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,  "raw_data", "misc", "GEOSChem.StateMet.20190101_0000z.nc4") )
 da = ds[miscer[ploter]]
@@ -54,7 +60,7 @@ damean.plot(ax=ax,
 # ax.set_title("Leaf Area Index", fontsize=14)
 
 
-plt.savefig(os.path.join(os.path.dirname(__file__), '..', 'backgroundgraphs', 'miscellaneous_figures', f'{plotargs[ploter][2]}.svg'))
+plt.savefig(os.path.join(os.path.dirname(__file__), '..', 'backgroundgraphs', 'miscellaneous_figures', f'{plotargs[ploter][2]}.png'), dpi=300)
 
 # Uncomment to display the plots on screen
 # plt.show()
--- backgroundgraphs/leafindexjuly.py
+++ backgroundgraphs/leafindexjuly.py
@@ -1,4 +1,5 @@
 # Import modules
+import matplotlib
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
@@ -9,14 +10,20 @@ import numpy as np
 
 
 miscer = ["Met_LAI", "Met_PHIS"]
-plotargs = [["Leaf Area Index (July)", "BuGn", "leafareaindexjuly"], ["Surface Geopotential Height", "gist_earth", "altitudejuly"]]
+plotargs = [["Leaf Area Index (July)", "BuGn", "leafareaindexjuly"], ["Surface Geopotential Height [m]", "gist_earth", "altitudejuly"]]
 
 # Which one would you like to plot: 0 Leaf Index, 1 Altitude
-ploter = 1
+ploter = 0
 
 # For loop to iterate through each type and through each variable withing each type
 fig, ax = plt.subplots(1, 1, figsize= [9, 7], subplot_kw={"projection": ccrs.EqualEarth(central_longitude=10)})
 plt.tight_layout()
+
+font = {'family' : 'DejaVu Sans',
+        'size'   : 20}
+
+matplotlib.rc('font', **font)
+
 
 ds = xr.open_dataset( os.path.join(os.path.dirname(__file__), ".." ,  "raw_data", "misc", "GEOSChem.StateMet.20190701_0000z.nc4") )
 da = ds[miscer[ploter]]
@@ -54,7 +61,7 @@ damean.plot(ax=ax,
 # ax.set_title("Leaf Area Index", fontsize=14)
 
 
-plt.savefig(os.path.join(os.path.dirname(__file__), '..', 'backgroundgraphs', 'miscellaneous_figures', f'{plotargs[ploter][2]}.svg'))
+plt.savefig(os.path.join(os.path.dirname(__file__), '..', 'backgroundgraphs', 'miscellaneous_figures', f'{plotargs[ploter][2]}.png'), dpi = 300)
 
 # Uncomment to display the plots on screen
 # plt.show()
